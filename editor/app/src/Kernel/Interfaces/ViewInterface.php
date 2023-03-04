9e28c8ac5246819b6f551c5629931ed881456b92
Ernesto Garrido <wdf.woodfox@gmail.com>
2023-03-04 14:23:00 +0000

    Adds comments

--- editor/app/src/Kernel/Interfaces/ViewInterface.php
+++ editor/app/src/Kernel/Interfaces/ViewInterface.php
@@ -4,9 +4,43 @@ namespace Easypage\Kernel\Interfaces;
 
 interface ViewInterface
 {
+    /**
+     * Enables or disable views cache
+     *
+     * @param  mixed $enabled
+     * @return void
+     */
     public function setCacheEnabled(bool $enabled): void;
+
+    /**
+     * Sets templates cache location 
+     *
+     * @param  mixed $path
+     * @return void
+     */
     public function setCachePath(string $path): void;
+
+    /**
+     * Renders template 
+     *
+     * @param  mixed $template_name
+     * @param  mixed $data
+     * @return string
+     */
     public function render(string $template_name, array $data = []): string;
+
+    /**
+     * Sets templates location
+     *
+     * @param  mixed $path
+     * @return void
+     */
     public function setTemplatesPath(string $path): void;
+
+    /**
+     * Returns an instance of a View class 
+     *
+     * @return self
+     */
     public static function getInstance(): self;
 }
